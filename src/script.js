eebcae02304fabbb389af4de4ff8bbf62fe54298
lilian-deville <85389067+lilian-deville@users.noreply.github.com>
2021-07-02 14:39:53 +0200

    Week 5 homework
    also updated css
    cleaned js from week 4
    week 5 still a work in progress

--- src/script.js
+++ src/script.js
@@ -25,17 +25,6 @@ function displayCurrent() {
 }
 displayCurrent();
 
-//Feature #2 Add a search engine, when searching for a city (i.e. Paris), //
-//display the city name on the page after the user submits the form.//
-function handle(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#cityentry");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = cityInput.value;
-}
-let search = document.querySelector("form");
-search.addEventListener("submit", handle);
-
 //
 //Bonus Feature: Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit.
 //When clicking on it, it should convert the temperature to Fahrenheit. When clicking on Celsius, it should convert it back to Celsius.//
@@ -58,5 +47,44 @@ function convertToCelsius(event) {
 let celsius = document.querySelector("#celsius-link");
 celsius.addEventListener("click", convertToCelsius);
 
+//Week 5: when a user searches for a city (example: New York), it should display the name of the city on the result page and the current temperature of the city.//
+function getTemp(response) {
+  let cityInputNew = document.querySelector("#currentLocation");
+  let city = response.data.name;
+  cityInputNew.innerHTML = `${city.value}`;
 
+  let temperature = document.querySelector(".currenttemp");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+}
+console.log(getTemp);
 
+function displayInputTemp(event) {
+  event.preventDefault();
+  let searchCity = document.querySelector("#cityentry");
+  let cityHeading = document.querySelector("#currentLocation");
+  cityHeading.innerHTML = `${searchCity.value}`;
+
+  let cityId = `${searchCity.value}`;
+  let apiKey = "bddc5a8545983fb2fb2516d2150a111f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityId}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(getTemp);
+}
+let searchNow = document.querySelector("#searchForm");
+searchNow.addEventListener("submit", displayInputTemp);
+
+function currentPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "bddc5a8545983fb2fb2516d2150a111f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(getTemp);
+}
+
+function showPosition() {
+  navigator.geolocation.getCurrentPosition(currentPosition);
+}
+
+let location = document.querySelector("#current-location");
+location.addEventListener("click", showPosition);
+
+//Week 5 bonus:Add a Current Location button. When clicking on it, it uses the Geolocation API to get your GPS coordinates and display and the city and current temperature using the OpenWeather API.//
