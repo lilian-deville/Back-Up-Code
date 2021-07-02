543b9641b29070eb2aa0c69f5030f4f0d8822b1d
lilian-deville <85389067+lilian-deville@users.noreply.github.com>
2021-07-02 12:22:52 +0200

    minus week 5 for netlify upload

--- src/script.js
+++ src/script.js
@@ -58,40 +58,5 @@ function convertToCelsius(event) {
 let celsius = document.querySelector("#celsius-link");
 celsius.addEventListener("click", convertToCelsius);
 
-//Week 5: when a user searches for a city (example: New York), it should display the name of the city on the result page and the current temperature of the city.//
-function inputCity (event) {
-  let cityInputNew = document.querySelector("#cityentry");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${cityInputNew.value}';
-}
-  function displayInputTemp(response) {
-    event.preventDefault();
-  let apiKey = "bddc5a8545983fb2fb2516d2150a111f";
-  let id = cityentry.value;
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&units=${units}`;
-  api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}
-  }
-let temperature = Math.round(response.data.main.temp);
-  console.log(apiUrl);
-let displayapitemp = document.querySelector(".currenttemp")
- let weatherDescription = response.data.weather[0].description;
-    let temperatureElement = document.querySelector(".temperature");
-    let weatherDescriptionElement = document.querySelector(
-      ".weather-condition"
-    );
-axios.get(apiUrl).then(showWeather);
- let  = document.querySelector("#currenttemp");
-  displayapitemp.innerHTML = `Currently ${temperature}`; 
-  }
-  let form = document.querySelector("form");
-form.addEventListener("submit",inputCity, displayInputTemp);
-    weatherDescriptionElement.innerHTML = weatherDescription;
-
-    //Week 5 bonus:Add a Current Location button. When clicking on it, it uses the Geolocation API to get your GPS coordinates and display and the city and current temperature using the OpenWeather API.//
-function selectstar(event) {
-let link to above = 
-}
-button.addEventListener("submit", starone);
 
 
