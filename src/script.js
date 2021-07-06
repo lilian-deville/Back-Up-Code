a534b162454cdbc2d03f7901ce1a70cffa21ec8e
lilian-deville <85389067+lilian-deville@users.noreply.github.com>
2021-07-06 16:40:07 +0200

    Troubleshooting js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,4 @@
-//Feature #1 display the current date and time using JavaScript: Tuesday 16:00
-
-function displayCurrentDay() {
+function displayCurrent() {
   let now = new Date();
   let days = [
     "Sunday",
@@ -20,50 +18,35 @@ function displayCurrentDay() {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  return `${day}, ${hours}:${minutes}`;
 }
-let dateElement = document.querySelector("#currentDayTime");
-let currentTime = new Date();
-dateElement.innerHTML = displayCurrentDay(currentTime);
+let formatDate = `${day}, ${hours}:${minutes}`;
+document.getElementById("currentDayTime").innerHTML = formatDate;
 
-function handle(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#cityentry");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = cityInput.value;
-}
-let search = document.querySelector("form");
-search.addEventListener("submit", handle);
-
+//
 function getTemp(response) {
   console.log(response);
-  let cityInputNew = document.querySelector("#currentLocation");
+  let cityInputNew = document.querySelector("#inputLocation");
+  let temperature = document.querySelector("#temperature");
   let city = response.data.name;
   cityInputNew.innerHTML = `${city}`;
-  let temperature = document.querySelector(".currenttemp");
   temperature.innerHTML = Math.round(response.data.main.temp);
 }
-
-//Feature #2 Add a search engine, when searching for a city (i.e. Paris), //
-//display the city name on the page after the user submits the form.//
-
-//get from week 7//
-
-//Week 5: when a user searches for a city (example: New York), it should display the name of the city on the result page and the current temperature of the city.//
-
 function displayInputTemp(event) {
   event.preventDefault();
   let searchCity = document.querySelector("#cityentry");
-  let cityHeading = document.querySelector("#currentLocation");
+  let cityHeading = document.querySelector("#inputLocation");
   cityHeading.innerHTML = `${searchCity.value}`;
 
   let cityId = `${searchCity.value}`;
-  let apiKey = "bddc5a8545983fb2fb2516d2150a111f";
+  let apiKey = "be9debd96f41247fd5df628d8a693a73";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityId}&units=metric`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(getTemp);
 }
+
 let searchNow = document.querySelector("#searchForm");
 searchNow.addEventListener("submit", displayInputTemp);
+
+//
 
 function currentPosition(position) {
   let lat = position.coords.latitude;
@@ -77,8 +60,9 @@ function showPosition() {
   navigator.geolocation.getCurrentPosition(currentPosition);
 }
 
-let location = document.querySelector("#current-location");
-location.addEventListener("subm", showPosition);
+let location = document.querySelector("#inputlocation");
+location.addEventListener("submit", showPosition);
+//up to here no console errors//
 
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
@@ -96,22 +80,18 @@ function displayCelsiusTemperature(event) {
   temperatureElement = Math.round(celsiusTemperature);
 }
 
-//new code to sort//
-
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySlectory("#currentLocation);
-  let descriptionElement = document.querySlectory("#description");
-  let humidityElement = document.querySlectory("#humidity");
-  let windElement = document.querySlectory("#wind");
-  let dateElement = document.querySlectory("#date");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
@@ -133,7 +113,7 @@ function search(city) {
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
-  search(cityInputElement.vale);
+  search(cityInputElement.value);
 }
 search("New York");
 
